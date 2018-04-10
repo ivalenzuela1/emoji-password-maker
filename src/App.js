b47fbbdf64f49d5e8d1833e3984964a450fbce5a
Craig Rodrigues <PagingCraig@gmail.com>
2018-04-10 06:28:28 -0400

    Emoji Password Maker Init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import emojis from './data/emoji.json';
+
+console.log(emojis);
+
+const App = () => {
+    return (
+        <div>
+            <h1>Hello, world!</h1>
+        </div>
+    );
+};
+
+export default App;
